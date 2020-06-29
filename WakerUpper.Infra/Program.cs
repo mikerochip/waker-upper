5c1b44a45431c0972916adb89ae8bf1090d63994
Mike Schweitzer <mschweitzer@gmail.com>
2020-06-29 11:37:22 -0700

    Added a dummy Pulumi Infra project
    * Hoping to replace the CodePipeline I built in the console with this

---
+++ WakerUpper.Infra/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Pulumi;
+
+namespace WakerUpper.Infra
+{
+    internal static class Program
+    {
+        private static Task<int> Main() => Deployment.RunAsync<MyStack>();
+    }
+}
